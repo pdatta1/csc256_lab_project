8286486a86ca9a9a508a93288a132933bb1c2545
Edison R Kaulfuss <104332031+ekaulfuss@users.noreply.github.com>
2023-10-22 01:52:37 -0400

    Barebones working login
    Login values are hardcoded as discussed for minimalist approach for first iteration. Successful login redirects to "/list" can be changed as needed depending on how the YourList.py file is developed.

--- Login.py
+++ Login.py
@@ -1,23 +1,33 @@
 # Basic framework for login request, single hardcoded user should be fine for
 # initial sprint to KISS
 
-from bottle import get, post, request # or route
+from bottle import run, get, post, request, redirect
+import YourList
 
-@get('/login') # or @route('/login')
+@get('/') 
 def login():
     return '''
-        <form action="/login" method="post">
-            Username: <input name="username" type="text" />
-            Password: <input name="password" type="password" />
+        <form action="/" method="post">
+            Username: <input name="username" type="text" /><br>
+            Password: <input name="password" type="password" /><br>
             <input value="Login" type="submit" />
         </form>
     '''
 
-@post('/login') # or @route('/login', method='POST')
+@post('/')
 def do_login():
     username = request.forms.get('username')
     password = request.forms.get('password')
     if check_login(username, password):
-        return "<p>Your login information was correct.</p>"
+        redirect("/list")
+        #  return "<p>Your login information was correct.</p>"
     else:
         return "<p>Login failed.</p>"
+    
+def check_login(username, password):
+    if username == "User" and password == "Pa55w0rd":
+        return True
+    else:
+        return False
+    
+run(host='localhost', port=8080, debug=True)
