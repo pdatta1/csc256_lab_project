4078b9ba9f08ac53cf1accf0e56efef39319a73b
Edison R Kaulfuss <104332031+ekaulfuss@users.noreply.github.com>
2023-10-24 04:54:58 -0400

    Functional todo app
    Uses session to handle the todo list using beaker.  Use "pip install beaker" to add this module to your environment.

--- ItemUpdate.py
+++ ItemUpdate.py
@@ -1,2 +1,73 @@
 # functionality to add new items with a blank list and
 # update with the page loaded from the list
+import bottle
+from bottle import route, get, post, request, run, redirect
+from beaker.middleware import SessionMiddleware
+import YourList
+import Login
+
+# Creates form allowing user to enter a new task
+# GET and POST are combined GET is a simple form, POST accessess the session adds the item to a list then readds the list item to the session
+@bottle.route('/newItems', method=['GET', 'POST'])
+def new_item():
+    if request.method == 'GET':
+        return '''
+            <form action="/newItems" method="post">
+                Task: <input name = "todo" type="text" />
+                <input value="Add" type="submit" />
+            </form>
+        '''
+    elif request.method == 'POST':
+        session = bottle.request.environ.get('beaker.session')
+        if 'todo_list' in session:
+            todo_list = session['todo_list']
+            taskToAdd = request.forms.get('todo')
+            todo_list.append(taskToAdd)
+            session['todo_list'] = todo_list
+        else:
+            todo_list = []
+            session['todo_list'] = todo_list
+        redirect('/list')
+
+# Create form allowing user to update existing task
+# Same as newItems just with form preloaded, POST sets existing item instead of append
+@bottle.route('/updateItems', method=['GET', 'POST'])
+def update_item():
+    todo_item = int(request.query.get('todo_item'))
+    if request.method == 'GET':
+        session = bottle.request.environ.get('beaker.session')
+        if 'todo_list' in session:
+            todo_list = session['todo_list']
+        else:
+            todo_list = []
+        return f'''
+            <form action="/updateItems?todo_item={todo_item}" method="post">
+                Task: <input name = "todo" value = "{todo_list[todo_item]}" type="text" />
+                <input value="Update" type="submit" />
+            </form>
+        '''
+    elif request.method == 'POST':
+        session = bottle.request.environ.get('beaker.session')
+        if 'todo_list' in session:
+            todo_list = session['todo_list']
+            taskToAdd = request.forms.get('todo')
+            todo_list[todo_item] = (taskToAdd)
+            session['todo_list'] = todo_list
+        else:
+            todo_list = []
+            session['todo_list'] = todo_list
+        redirect('/list')
+
+# deleteItems only has one method since it doesn't have a display, pops indicated item from list pulled from session before readding it to the session
+@bottle.route('/deleteItems', method=['GET'])
+def delete_item():
+    session = bottle.request.environ.get('beaker.session')
+    todo_item = int(request.query.get('todo_item'))
+    if 'todo_list' in session:
+        todo_list = session['todo_list']
+        todo_list.pop(todo_item)
+        session['todo_list'] = todo_list
+    else:
+        todo_list = []
+        session['todo_list'] = todo_list
+    redirect('/list')    
--- Login.py
+++ Login.py
@@ -1,12 +1,25 @@
 # Basic framework for login request, single hardcoded user should be fine for
 # initial sprint to KISS
 
-from bottle import run, get, post, request, redirect
-import YourList
+import bottle
+from bottle import Bottle, route, run, get, post, request, redirect
+from beaker.middleware import SessionMiddleware
 
-@get('/') 
+# Load session before calling imports for the other pages
+session_opts = {
+    'session.type': 'memory',
+    'session.auto': True,
+}
+# Once you wrap the bottle app in the SessionMiddleware app you'll have to reference bottle directly when you make a bottle call (like bottle.route())
+app  = SessionMiddleware(bottle.app(), session_opts)
+
+import YourList
+import ItemUpdate
+
+# Simple login function, use f string as best practice for dynamic strings
+@bottle.get('/') 
 def login():
-    return '''
+    return f'''
         <form action="/" method="post">
             Username: <input name="username" type="text" /><br>
             Password: <input name="password" type="password" /><br>
@@ -14,20 +27,25 @@ def login():
         </form>
     '''
 
-@post('/')
+# Successful login causes a redirect, failure displays fail message
+@bottle.post('/')
 def do_login():
     username = request.forms.get('username')
     password = request.forms.get('password')
     if check_login(username, password):
-        redirect("/list")
+        redirect('/list')
         #  return "<p>Your login information was correct.</p>"
     else:
         return "<p>Login failed.</p>"
     
+# Login info is hard coded at this point
 def check_login(username, password):
+    # Login detail are currently hardcoded for this sprint
     if username == "User" and password == "Pa55w0rd":
         return True
     else:
         return False
-    
-run(host='localhost', port=8080, debug=True)
+
+# command that causes the bottle server to run app=app causes the function to see the SessionManager app as a bottle app
+if __name__ == '__main__':
+    bottle.run(host='localhost', port=8080, app=app, debug=True)
