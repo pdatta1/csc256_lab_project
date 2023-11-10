834fcb1c9d69475a020722c55c25c9db2e3025fc
Jordan Manus <jlmanus17@gmail.com>
2023-11-10 15:38:50 -0500

    added template to login.py.

--- Login.py
+++ Login.py
@@ -1,51 +1,49 @@
-# Basic framework for login request, single hardcoded user should be fine for
-# initial sprint to KISS
-
-import bottle
-from bottle import Bottle, route, run, get, post, request, redirect
+# import bottle
+from bottle import route, run, request, template, post, redirect, static_file, app, get 
 from beaker.middleware import SessionMiddleware
+
 
 # Load session before calling imports for the other pages
 session_opts = {
     'session.type': 'memory',
     'session.auto': True,
 }
+
 # Once you wrap the bottle app in the SessionMiddleware app you'll have to reference bottle directly when you make a bottle call (like bottle.route())
-app  = SessionMiddleware(bottle.app(), session_opts)
+app = SessionMiddleware(app(), session_opts)
 
-import YourList
+# from YourList import your_list
 import ItemUpdate
+import YourList
 
-# Simple login function, use f string as best practice for dynamic strings
-@bottle.get('/') 
+
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./views/static')
+
+
+@route('/')
 def login():
-    return f'''
-        <form action="/" method="post">
-            Username: <input name="username" type="text" /><br>
-            Password: <input name="password" type="password" /><br>
-            <input value="Login" type="submit" />
-        </form>
-    '''
+    return template('login')
 
-# Successful login causes a redirect, failure displays fail message
-@bottle.post('/')
+
+@post('/')
 def do_login():
     username = request.forms.get('username')
     password = request.forms.get('password')
+
     if check_login(username, password):
         redirect('/list')
-        #  return "<p>Your login information was correct.</p>"
     else:
         return "<p>Login failed.</p>"
-    
-# Login info is hard coded at this point
+
+
 def check_login(username, password):
-    # Login detail are currently hardcoded for this sprint
-    if username == "User" and password == "Pa55w0rd":
+    if username == "User" and password == "password":
         return True
     else:
         return False
+    
 
-# command that causes the bottle server to run app=app causes the function to see the SessionManager app as a bottle app
 if __name__ == '__main__':
-    bottle.run(host='localhost', port=8080, app=app, debug=True)
+    run(host='localhost', port=8080, app=app, debug=True, reloader=True)
