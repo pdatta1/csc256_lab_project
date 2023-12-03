dd7edd71772b2f7b6c2d0d7de1a7984bd563521c
aliyah-robinson <116529635+aliyah-robinson@users.noreply.github.com>
2023-12-03 17:27:20 -0500

    Update test_todolist_api.py

--- tests/api/test_login_api.py
+++ tests/api/test_login_api.py
@@ -8,31 +8,12 @@ import requests
 ENDPOINT = "http://localhost:8080/"
 
 
-def test_can_call_endpoint():
-    response = requests.get(ENDPOINT)
-    assert response.status_code == 200
-
-
-def test_validLogin():
-    login = {
-        "content": "login test content",
-        "user_id": "User",
-        "password": "password",
-        "is_done": False,
-    }
-    response = requests.post(ENDPOINT + "/getjson", json=login)
+def test_get_todo_list():
+    response = requests.get(ENDPOINT + "/getjson")
     assert response.status_code == 200
 
     data = response.json()
     print(data)
 
-    # this part forward I got from the video I watched. IDK how to apply what he did to ours
 
-    task_id = data["task"]["task_id"]
-    get_task_response = requests.get(ENDPOINT + f"/getjson/{task_id}")
 
-    assert get_task_response.status_code == 200
-    get_task_data = get_task_response.json()
-    assert get_task_data["content"] == login["content"]
-    assert get_task_data["user_id"] == login["user_id"]
-    print(get_task_data)
