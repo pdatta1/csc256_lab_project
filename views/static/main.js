1889409df1b0031bb5a42a100631fd75f6366b01
Edison R Kaulfuss <104332031+ekaulfuss@users.noreply.github.com>
2023-12-05 07:32:41 -0500

    Update main.js
    Update for api addition

--- views/static/main.js
+++ views/static/main.js
@@ -23,3 +23,29 @@ function processForm(action) {
 
     form.submit();
 }
+
+function processApiForm(action) {
+    let form = document.getElementById('todo_form');
+    let selectedRadio = document.querySelector('input[name="todo_item"]:checked');
+    let selectedValue = selectedRadio ? selectedRadio.value : null;
+
+    switch (action) {
+        case 'add':
+            form.action = '/apiNewItems';
+            break;
+        case 'update': form.action = '/apiUpdateItems?todo_item=' + selectedValue;
+            break;
+        case 'delete':
+            form.action = '/apiDeleteItems?todo_item=' + selectedValue;
+            break;
+        case 'sort':
+            form.action = '/apiSortItems';
+            break;
+        case 'json':
+            form.action = '/apiGetJson';
+            break;
+
+    }
+
+    form.submit();
+}
